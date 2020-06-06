6491ea5dcd15f5b86a8471bc5b004f6a04343d99
DARSAN HIRANI URF BABAYAGI <53813630+BABAYAGI@users.noreply.github.com>
2020-06-06 12:16:06 +1000

    updates
    working

--- Sum.java
+++ Sum.java
@@ -1,7 +1,31 @@
-public class Sum {
-    public static public static void main(String[] args) {
-        
-        System.out.println(args[0]);
-    }
-    
+/*
+ * Class Name:    Sum
+ *
+ * Author: Darsan
+ * Creation Date: Saturday, June 06 2020, 09:35 
+ * Last Modified: Saturday, June 06 2020, 11:58
+ * 
+ * Class Description:
+ *
+ */
+
+public class Sum
+{
+ public static void main(String[] args)
+
+ {
+  int total = 0;
+  int loop ;
+  for(int i=0;i<args.length;i++)
+  {
+   int string2int = Integer.parseInt(args[i]);
+   total = total + string2int;
+
+  }
+  System.out.println(total);     
+      
+   
+ 
+      
+}
 }
